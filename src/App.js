cd802d3bd152d73b32f60db8d6d72c9e17467f1a
zaycevarthur <zaycevarthur@gmail.com>
2023-05-18 18:08:07 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import "./App.css";
+import "./scss/app.scss";
+import Header from "./components/Header/Header";
+import Categories from "./components/Categories/Categories";
+import Sort from "./components/Sort/Sort";
+import PizzaBlock from "./components/PizzaBlock/PizzaBlock";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="wrapper">
+        <Header />
+        <div className="content">
+          <div className="container">
+            <div className="content__top">
+              <Categories />
+              <Sort />
+            </div>
+            <h2 className="content__title">Все пиццы</h2>
+            <div className="content__items">
+              <PizzaBlock />
+              <PizzaBlock />
+              <PizzaBlock />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
