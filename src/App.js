c6625d1e58a39c293a02f27491627fcd74d5d5f3
zaycevarthur <zaycevarthur@gmail.com>
2023-05-23 20:36:36 +0500

    5 commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import "./App.css";
+import React, { useState, useEffect } from "react";
+
 import "./scss/app.scss";
 import Header from "./components/Header/Header";
 import Categories from "./components/Categories/Categories";
 import Sort from "./components/Sort/Sort";
 import PizzaBlock from "./components/PizzaBlock/PizzaBlock";
-import pizzas from "./assets/pizzas.json";
+import Skeleton from "./components/PizzaBlock/Skeleton";
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://646c8c187b42c06c3b2b7bcd.mockapi.io/items")
+      .then((res) => res.json())
+      .then((arr) => {
+        setItems(arr);
+        setIsLoading(false);
+      });
+  }, []);
+
   return (
     <div className="App">
       <div className="wrapper">
@@ -19,9 +32,9 @@ function App() {
             </div>
             <h2 className="content__title">Все пиццы</h2>
             <div className="content__items">
-              {pizzas.map((obj) => (
-                <PizzaBlock key={obj.id} {...obj} />
-              ))}
+              {isLoading
+                ? [...new Array(6)].map((_, i) => <Skeleton key={i} />)
+                : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
             </div>
           </div>
         </div>
